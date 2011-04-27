deb6a6096a9a4204dc074e47ceb36e1e6910da28
benducke <benducke@d80a0976-42e5-4521-8f91-9f5439ad514b>
2011-04-27 09:01:37 +0000

    git-svn-id: https://gvsigce.svn.sourceforge.net/svnroot/gvsigce/trunk@232 d80a0976-42e5-4521-8f91-9f5439ad514b

---
+++ extensions/extDataLocator/src/com/iver/gvsig/datalocator/AllTests.java
@@ -0,0 +1,16 @@
+package com.iver.gvsig.datalocator;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+public class AllTests {
+
+	public static Test suite() {
+		TestSuite suite = new TestSuite("Test for com.iver.gvsig.datalocator");
+		//$JUnit-BEGIN$
+
+		//$JUnit-END$
+		return suite;
+	}
+
+}
