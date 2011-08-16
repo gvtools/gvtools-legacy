8b428a56016a919b37fff2d14bccfbeefb004e25
benducke <benducke@d80a0976-42e5-4521-8f91-9f5439ad514b>
2011-08-16 17:36:34 +0000

    Synchronizing https://svn.forge.osor.eu/svn/gvsig-desktop, commit #33294
    git-svn-id: https://gvsigce.svn.sourceforge.net/svnroot/gvsigce/trunk@468 d80a0976-42e5-4521-8f91-9f5439ad514b

--- libraries/libFMap/src/com/iver/cit/gvsig/fmap/operations/strategies/DBStrategy.java
+++ libraries/libFMap/src/com/iver/cit/gvsig/fmap/operations/strategies/DBStrategy.java
@@ -84,102 +84,102 @@ public class DBStrategy extends DefaultStrategy {
 	}
 
 
-    /* (non-Javadoc)
-     * @see com.iver.cit.gvsig.fmap.operations.strategies.Strategy#draw(java.awt.image.BufferedImage, java.awt.Graphics2D, com.iver.cit.gvsig.fmap.ViewPort, com.iver.cit.gvsig.fmap.operations.Cancellable)
-     */
-    public void draw(BufferedImage image, Graphics2D g, ViewPort viewPort, Cancellable cancel) throws ReadDriverException {
-        // Nos aprovechamos del SQL para lanzar la consulta
-        // teniendo en cuenta el boundingbox que toca.
-        FLyrVect lyr = (FLyrVect) getCapa();
-        ISpatialDB dbAdapter = (ISpatialDB) ((SingleLayer) capa).getSource();
-        //IVectorialDatabaseDriver dbDriver = (IVectorialDatabaseDriver) dbAdapter.getDriver();
-        try {
+	/* (non-Javadoc)
+	 * @see com.iver.cit.gvsig.fmap.operations.strategies.Strategy#draw(java.awt.image.BufferedImage, java.awt.Graphics2D, com.iver.cit.gvsig.fmap.ViewPort, com.iver.cit.gvsig.fmap.operations.Cancellable)
+	 */
+	public void draw(BufferedImage image, Graphics2D g, ViewPort viewPort, Cancellable cancel) throws ReadDriverException {
+		// Nos aprovechamos del SQL para lanzar la consulta
+		// teniendo en cuenta el boundingbox que toca.
+		FLyrVect lyr = (FLyrVect) getCapa();
+		ISpatialDB dbAdapter = (ISpatialDB) ((SingleLayer) capa).getSource();
+		//IVectorialDatabaseDriver dbDriver = (IVectorialDatabaseDriver) dbAdapter.getDriver();
+		try {
 			dbAdapter.start();
-	        Selectable selectable=lyr.getRecordset();
+			Selectable selectable=lyr.getRecordset();
 			ICoordTrans ct = lyr.getCoordTrans();
 			FBitSet bitSet = selectable.getSelection();
 
-	        String strEPSG = viewPort.getProjection().getAbrev();
-	        // TODO: EXPLORAR LAS LEYENDAS PARA SABER LOS CAMPOS QUE VAMOS
-	        // A NECESITAR RECUPERAR, PARA INCLUIR EN LA CONSULTA SOLO
-	        // AQUELLOS QUE VAMOS A NECESITAR. CON ESO GANAREMOS VELOCIDAD.
-	        // Ejemplo:
-	        // En ArcSDE:
-	        // con Vias cogiendo un campo solo: 5 segundos
-	        // con todos los campos de Vias: 11 segundos.
-	        // => MODIFICAR EL getFeatureIterator para que admita los nombres
-	        // de los campos además del rectángulo que pides.
-	        String[] usedFields = null;
-	        IClassifiedVectorLegend l = null;
-	        if (lyr.getLegend() instanceof IClassifiedVectorLegend)
-	        {
-	            l = (IClassifiedVectorLegend) lyr.getLegend();
+			String strEPSG = viewPort.getProjection().getAbrev();
+			// TODO: EXPLORAR LAS LEYENDAS PARA SABER LOS CAMPOS QUE VAMOS
+			// A NECESITAR RECUPERAR, PARA INCLUIR EN LA CONSULTA SOLO
+			// AQUELLOS QUE VAMOS A NECESITAR. CON ESO GANAREMOS VELOCIDAD.
+			// Ejemplo:
+			// En ArcSDE:
+			// con Vias cogiendo un campo solo: 5 segundos
+			// con todos los campos de Vias: 11 segundos.
+			// => MODIFICAR EL getFeatureIterator para que admita los nombres
+			// de los campos además del rectángulo que pides.
+			String[] usedFields = null;
+			IClassifiedVectorLegend l = null;
+			if (lyr.getLegend() instanceof IClassifiedVectorLegend)
+			{
+				l = (IClassifiedVectorLegend) lyr.getLegend();
 
-		        usedFields = l.getClassifyingFieldNames();
+				usedFields = l.getClassifyingFieldNames();
 
-	        }
+			}
 
-	        Rectangle2D rectAux = viewPort.getAdjustedExtent();
-	        if (ct != null) {
-	            ICoordTrans invertedCT = ct.getInverted();
-	            rectAux = invertedCT.convert(rectAux);
-	        }
-
-
-
-	        IFeatureIterator geomIt = dbAdapter.getFeatureIterator(rectAux, strEPSG, usedFields);
-	        if (geomIt == null)
-	        {
-	            // dbAdapter.stop();
-	            return;
-	        }
+			Rectangle2D rectAux = viewPort.getAdjustedExtent();
+			if (ct != null) {
+				ICoordTrans invertedCT = ct.getInverted();
+				rectAux = invertedCT.convert(rectAux);
+			}
 
 
-	        DriverAttributes attr = dbAdapter.getDriverAttributes();
-	        boolean bMustClone = false;
-	        if (attr != null)
-	        {
-	            if (attr.isLoadedInMemory())
-	            {
-	                bMustClone = attr.isLoadedInMemory();
-	            }
-	        }
+
+			IFeatureIterator geomIt = dbAdapter.getFeatureIterator(rectAux, strEPSG, usedFields);
+			if (geomIt == null)
+			{
+				// dbAdapter.stop();
+				return;
+			}
 
 
-	        int i;
-	        ISymbol symbol;
-	        SpatialCache cache = lyr.getSpatialCache();
-	        cache.clearAll();
-	        while (geomIt.hasNext())
-	        {
-	        	if (cancel.isCanceled()) {
-	        	    geomIt.closeIterator();
-	        		break;
-	        	}
-	        	IFeature feat = geomIt.next();
-	        	if (feat == null)
-	        	{
-	        		continue;
-	        	}
-	            IGeometry geom = feat.getGeometry();
+			DriverAttributes attr = dbAdapter.getDriverAttributes();
+			boolean bMustClone = false;
+			if (attr != null)
+			{
+				if (attr.isLoadedInMemory())
+				{
+					bMustClone = attr.isLoadedInMemory();
+				}
+			}
 
-	            // System.out.println("PINTANDO FEATURE " + feat.getID());
 
-	            if (ct != null) {
-	                if (bMustClone)
-	                    geom = geom.cloneGeometry();
-	                geom.reProject(ct);
-	            }
+			int i;
+			ISymbol symbol;
+			SpatialCache cache = lyr.getSpatialCache();
+			cache.clearAll();
+			while (geomIt.hasNext())
+			{
+				if (cancel.isCanceled()) {
+					geomIt.closeIterator();
+					break;
+				}
+				IFeature feat = geomIt.next();
+				if (feat == null)
+				{
+					continue;
+				}
+				IGeometry geom = feat.getGeometry();
 
-	            i = dbAdapter.getRowIndexByFID(feat);
-	            // System.out.println("Antes de pintar " + i);
-	            // Value val = feat.getAttribute(0);
-	            symbol = l.getSymbolByFeature(feat);
-	            // symbol = l.getSymbol(i);
-	            if (symbol == null) continue;
-	            if (bitSet.get(i)) {
-	                symbol = symbol.getSymbolForSelection();
-	            }
+				// System.out.println("PINTANDO FEATURE " + feat.getID());
+
+				if (ct != null) {
+					if (bMustClone)
+						geom = geom.cloneGeometry();
+					geom.reProject(ct);
+				}
+
+				i = dbAdapter.getRowIndexByFID(feat);
+				// System.out.println("Antes de pintar " + i);
+				// Value val = feat.getAttribute(0);
+				symbol = l.getSymbolByFeature(feat);
+				// symbol = l.getSymbol(i);
+				if (symbol == null) continue;
+				if (bitSet.get(i)) {
+					symbol = symbol.getSymbolForSelection();
+				}
 
 				if (lyr.isSpatialCacheEnabled()) {
 					if (cache.getMaxFeatures() >= cache.size()) {
@@ -188,157 +188,157 @@ public class DBStrategy extends DefaultStrategy {
 					}
 				}
 
-	            // symbol = l.getDefaultSymbol();
-	            geom.drawInts(g,viewPort, symbol, null);
+				// symbol = l.getDefaultSymbol();
+				geom.drawInts(g,viewPort, symbol, null);
 
-	        }
-	        dbAdapter.stop();
-		} catch (InitializeDriverException e) {
-			throw new ReadDriverException(getCapa().getName(),e);
-		}
-
-    }
-    /**
-     * Processes features of layer whose geometries would
-     * intersect rectangle passed as param by calling visitor's
-     * visit method.
-     *
-     * FIXME FeatureVisitor is designed to work with memory and
-     * file drivers (visit(IGeometry, index), and to work with
-     * geometries (thats the reason for not to create Values, instead
-     * of pass an index)
-     *
-     * But DBDrivers recovers IGeometry and Values in dbAdapter.getFeatureIterator
-     * method. We must add a visit(Feature) method to FeatureVisitor.
-     * @throws ExpansionFileReadException
-     *
-     *
-     */
-    public void process(FeatureVisitor visitor, Rectangle2D rect, CancellableMonitorable cancel)
-    					throws ReadDriverException, ExpansionFileReadException, VisitorException{
-    	 FLyrVect lyr = (FLyrVect) getCapa();
-         ISpatialDB dbAdapter = (ISpatialDB) ((SingleLayer) capa).getSource();
-         IVectorialDatabaseDriver dbDriver = (IVectorialDatabaseDriver) dbAdapter.getDriver();
-       	try {
-			dbAdapter.start();
-		} catch (InitializeDriverException e) {
-			throw new ReadDriverException(getCapa().getName(),e);
-		}
- 			ICoordTrans ct = lyr.getCoordTrans();
- 			String strEPSG = lyr.getProjection().getAbrev();
- 	        Rectangle2D rectAux = rect;
- 	        if (ct != null) {
- 	            ICoordTrans invertedCT = ct.getInverted();
- 	            rectAux = invertedCT.convert(rectAux);
- 	        }
- 	        IFeatureIterator geomIt = dbAdapter.getFeatureIterator(rectAux, strEPSG, null);
- 	        if (geomIt == null){
- 	            return;
- 	        }
- 	        DriverAttributes attr = dbAdapter.getDriverAttributes();
- 	        boolean bMustClone = false;
- 	        if (attr != null)
- 	        {
- 	            if (attr.isLoadedInMemory())
- 	            {
- 	                bMustClone = attr.isLoadedInMemory();
- 	            }//if
- 	        }//if
- 	        int i;
- 	        while (geomIt.hasNext())
- 	        {
- 	        	if(cancel != null){
-					cancel.reportStep();
-				}
- 	        	IFeature feat = geomIt.next();
- 	            IGeometry geom = feat.getGeometry();
- 	            if (ct != null) {
- 	                if (bMustClone){
- 	                    geom = geom.cloneGeometry();
- 	                }//if
- 	                geom.reProject(ct);
- 	            }//if
- 	            i = dbDriver.getRowIndexByFID(feat);
- 	            if (geom.intersects(rect)){
- 	            	visitor.visit(geom, i);
- 	            }
- 	        }
- 	        dbAdapter.stop();
-    }
-
-	/* (non-Javadoc)
-	 * @see com.iver.cit.gvsig.fmap.operations.strategies.DefaultStrategy#queryByRect(java.awt.geom.Rectangle2D)
-	 */
-	public FBitSet queryByRect(Rectangle2D rect, CancellableMonitorable cancel) throws ReadDriverException, VisitorException {
-        // Nos aprovechamos del SQL para lanzar la consulta
-        // teniendo en cuenta el boundingbox que toca.
-        FLyrVect lyr = (FLyrVect) getCapa();
-        ISpatialDB dbAdapter = (ISpatialDB) ((SingleLayer) capa).getSource();
-//        IVectorialDatabaseDriver dbDriver = (IVectorialDatabaseDriver) dbAdapter.getDriver();
-        try {
-			dbAdapter.start();
-			ICoordTrans ct = lyr.getCoordTrans();
-			String strEPSG = lyr.getProjection().getAbrev();
-	        Rectangle2D rectAux = rect;
-	        if (ct != null) {
-	            ICoordTrans invertedCT = ct.getInverted();
-	            rectAux = invertedCT.convert(rectAux);
-	        }
-
-	        FBitSet selection = new FBitSet();
-
-	        IFeatureIterator geomIt = dbAdapter.getFeatureIterator(rectAux, strEPSG, null);
-	        if (geomIt == null)
-	        {
-	            return selection;
-	        }
-
-
-	        DriverAttributes attr = dbAdapter.getDriverAttributes();
-	        boolean bMustClone = false;
-	        if (attr != null)
-	        {
-	            if (attr.isLoadedInMemory())
-	            {
-	                bMustClone = attr.isLoadedInMemory();
-	            }
-	        }
-
-
-	        int i;
-
-	        while (geomIt.hasNext())
-	        {
-	        	if(cancel != null)
-	        	{
-	        		cancel.reportStep();
-	        		if(cancel.isCanceled())
-	        		{
-	        			dbAdapter.stop();
-	        			return selection;
-	        		}
-	        	}
-	        	IFeature feat = geomIt.next();
-	            IGeometry geom = feat.getGeometry();
-
-	            if (ct != null) {
-	                if (bMustClone)
-	                    geom = geom.cloneGeometry();
-	                geom.reProject(ct);
-	            }
-
-	            i = dbAdapter.getRowIndexByFID(feat);
-	            if (geom.intersects(rect))
-	            	selection.set(i, true);
-	        }
-	        dbAdapter.stop();
-
-	        return selection;
+			}
+			dbAdapter.stop();
 		} catch (InitializeDriverException e) {
 			throw new ReadDriverException(getCapa().getName(),e);
 		}
 
 	}
+	/**
+	 * Processes features of layer whose geometries would
+	 * intersect rectangle passed as param by calling visitor's
+	 * visit method.
+	 *
+	 * FIXME FeatureVisitor is designed to work with memory and
+	 * file drivers (visit(IGeometry, index), and to work with
+	 * geometries (thats the reason for not to create Values, instead
+	 * of pass an index)
+	 *
+	 * But DBDrivers recovers IGeometry and Values in dbAdapter.getFeatureIterator
+	 * method. We must add a visit(Feature) method to FeatureVisitor.
+	 * @throws ExpansionFileReadException
+	 *
+	 *
+	 */
+	public void process(FeatureVisitor visitor, Rectangle2D rect, CancellableMonitorable cancel)
+	throws ReadDriverException, ExpansionFileReadException, VisitorException{
+		FLyrVect lyr = (FLyrVect) getCapa();
+		if (visitor.start(lyr)){
+			ISpatialDB dbAdapter = (ISpatialDB) ((SingleLayer) capa).getSource();
+			IVectorialDatabaseDriver dbDriver = (IVectorialDatabaseDriver) dbAdapter.getDriver();
+			try {
+				dbAdapter.start();
+			} catch (InitializeDriverException e) {
+				throw new ReadDriverException(getCapa().getName(),e);
+			}
+			ICoordTrans ct = lyr.getCoordTrans();
+			String strEPSG = lyr.getProjection().getAbrev();
+			Rectangle2D rectAux = rect;
+			if (ct != null) {
+				ICoordTrans invertedCT = ct.getInverted();
+				rectAux = invertedCT.convert(rectAux);
+			}
+			IFeatureIterator geomIt = dbAdapter.getFeatureIterator(rectAux, strEPSG, null);
+			if (geomIt == null){
+				return;
+			}
+			DriverAttributes attr = dbAdapter.getDriverAttributes();
+			boolean bMustClone = false;
+			if (attr != null)
+			{
+				if (attr.isLoadedInMemory())
+				{
+					bMustClone = attr.isLoadedInMemory();
+				}//if
+			}//if
+			int i;
+			while (geomIt.hasNext())
+			{
+				if(cancel != null){
+					cancel.reportStep();
+				}
+				IFeature feat = geomIt.next();
+				IGeometry geom = feat.getGeometry();
+				if (ct != null) {
+					if (bMustClone){
+						geom = geom.cloneGeometry();
+					}//if
+					geom.reProject(ct);
+				}//if
+				i = dbDriver.getRowIndexByFID(feat);
+				if (geom.intersects(rect)){
+					visitor.visit(geom, i);
+				}
+			}
+			dbAdapter.stop();
+			visitor.stop(lyr);
+		}
+	}
+
+	/* (non-Javadoc)
+	 * @see com.iver.cit.gvsig.fmap.operations.strategies.DefaultStrategy#queryByRect(java.awt.geom.Rectangle2D)
+	 */
+	public FBitSet queryByRect(Rectangle2D rect, CancellableMonitorable cancel) throws ReadDriverException, VisitorException {
+		// Nos aprovechamos del SQL para lanzar la consulta
+		// teniendo en cuenta el boundingbox que toca.
+		FLyrVect lyr = (FLyrVect) getCapa();
+		ISpatialDB dbAdapter = (ISpatialDB) ((SingleLayer) capa).getSource();
+		//        IVectorialDatabaseDriver dbDriver = (IVectorialDatabaseDriver) dbAdapter.getDriver();
+		try {
+			dbAdapter.start();
+			ICoordTrans ct = lyr.getCoordTrans();
+			String strEPSG = lyr.getProjection().getAbrev();
+			Rectangle2D rectAux = rect;
+			if (ct != null) {
+				ICoordTrans invertedCT = ct.getInverted();
+				rectAux = invertedCT.convert(rectAux);
+			}
+
+			FBitSet selection = new FBitSet();
+
+			IFeatureIterator geomIt = dbAdapter.getFeatureIterator(rectAux, strEPSG, null);
+			if (geomIt == null)
+			{
+				return selection;
+			}
 
 
+			DriverAttributes attr = dbAdapter.getDriverAttributes();
+			boolean bMustClone = false;
+			if (attr != null)
+			{
+				if (attr.isLoadedInMemory())
+				{
+					bMustClone = attr.isLoadedInMemory();
+				}
+			}
+
+
+			int i;
+
+			while (geomIt.hasNext())
+			{
+				if(cancel != null)
+				{
+					cancel.reportStep();
+					if(cancel.isCanceled())
+					{
+						dbAdapter.stop();
+						return selection;
+					}
+				}
+				IFeature feat = geomIt.next();
+				IGeometry geom = feat.getGeometry();
+
+				if (ct != null) {
+					if (bMustClone)
+						geom = geom.cloneGeometry();
+					geom.reProject(ct);
+				}
+
+				i = dbAdapter.getRowIndexByFID(feat);
+				if (geom.intersects(rect))
+					selection.set(i, true);
+			}
+			dbAdapter.stop();
+
+			return selection;
+		} catch (InitializeDriverException e) {
+			throw new ReadDriverException(getCapa().getName(),e);
+		}
+	}
 }
