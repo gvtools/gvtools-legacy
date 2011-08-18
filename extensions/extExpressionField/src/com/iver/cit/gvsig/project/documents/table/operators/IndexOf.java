457f298bf7550454b1c5ef3bd49d2ee99831a121
benducke <benducke@d80a0976-42e5-4521-8f91-9f5439ad514b>
2011-08-18 09:37:44 +0000

    Synchronizing with https://svn.forge.osor.eu/svn/gvsig-desktop/, commit #35764
    git-svn-id: https://gvsigce.svn.sourceforge.net/svnroot/gvsigce/trunk@577 d80a0976-42e5-4521-8f91-9f5439ad514b

--- extensions/extExpressionField/src/com/iver/cit/gvsig/project/documents/table/operators/IndexOf.java
+++ extensions/extExpressionField/src/com/iver/cit/gvsig/project/documents/table/operators/IndexOf.java
@@ -22,7 +22,7 @@ public class IndexOf extends AbstractOperator{
 	public void eval(BSFManager interpreter) throws BSFException {
 //		interpreter.eval(ExpressionFieldExtension.BEANSHELL,null,-1,-1,"int indexOf(String value1,String value2){return value1.indexOf(value2);};");
 		interpreter.exec(ExpressionFieldExtension.JYTHON,null,-1,-1,"def indexOf(value1,value2):\n" +
-				"  return value1.index(value2)");
+				"  return value1.find(value2)");
 	}
 	public boolean isEnable() {
 		return (getType()==IOperator.STRING);
