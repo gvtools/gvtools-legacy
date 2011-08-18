577306eb10e987550dc015200aca0c5fbe123ffc
benducke <benducke@d80a0976-42e5-4521-8f91-9f5439ad514b>
2011-08-18 14:54:48 +0000

    Will replace this with the current version in OSOR SVN.
    git-svn-id: https://gvsigce.svn.sourceforge.net/svnroot/gvsigce/trunk@598 d80a0976-42e5-4521-8f91-9f5439ad514b

--- libraries/libRemoteServices/src/org/gvsig/remoteClient/RasterClient.java
+++
@@ -1,9 +0,0 @@
-
-package org.gvsig.remoteClient;
-
-/**
- * <p></p>
- * 
- */
-public abstract class RasterClient extends org.gvsig.remoteClient.RemoteClient implements org.gvsig.remoteClient.IRasterClient {
- }
