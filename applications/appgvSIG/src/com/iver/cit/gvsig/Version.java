afa8996d9eca2d4b0d966beddf75dcd98d0c97cf
volaya <volaya@d80a0976-42e5-4521-8f91-9f5439ad514b>
2011-11-29 09:22:11 +0000

    added isGvSIGCE() method
    git-svn-id: https://gvsigce.svn.sourceforge.net/svnroot/gvsigce/trunk@671 d80a0976-42e5-4521-8f91-9f5439ad514b

--- applications/appgvSIG/src/com/iver/cit/gvsig/Version.java
+++ applications/appgvSIG/src/com/iver/cit/gvsig/Version.java
@@ -4,119 +4,145 @@ import java.util.Properties;
 
 import org.apache.log4j.Logger;
 
-import com.iver.cit.gvsig.DEMO.SingleView;
 import com.iver.cit.gvsig.gui.panels.FPanelAbout;
 
 public class Version {
-	private static Logger logger = Logger.getLogger(Version.class.getName());
-	/**
-	 * @deprecated Use method getMajor 
-	 */
-	public static int MAJOR_VERSION_NUMBER = 0;
-	/**
-	 * @deprecated Use method getMinor 
-	 */
-	public static int MINOR_VERSION_NUMBER = 0;
-	/**
-	 * @deprecated Use method getRelease 
-	 */
-	public static int RELEASE_NUMBER = 0;
-	private static boolean loaded = false;
+   private static Logger  logger               = Logger.getLogger(Version.class.getName());
+   /**
+    * @deprecated Use method getMajor
+    */
+   @Deprecated
+   public static int      MAJOR_VERSION_NUMBER = 0;
+   /**
+    * @deprecated Use method getMinor
+    */
+   @Deprecated
+   public static int      MINOR_VERSION_NUMBER = 0;
+   /**
+    * @deprecated Use method getRelease
+    */
+   @Deprecated
+   public static int      RELEASE_NUMBER       = 0;
+   private static boolean loaded               = false;
 
-	private static String BUILD = null;
-	private static String STATE = null;
+   private static String  BUILD                = null;
+   private static String  STATE                = null;
 
-	static {
-		Version version = new Version();
-		version.loadVersion();
-	}
-	/**
-	 * @deprecated Use method getFormat 
-	 */
-	public static String format() {
+   static {
+      final Version version = new Version();
+      version.loadVersion();
+   }
 
-		Version version = new Version();
-		return version.getFormat();
-	}
 
-	/**
-	 * @deprecated Use method getLongFormat 
-	 */
-	public static String longFormat() {
+   /**
+    * @deprecated Use method getFormat
+    */
+   @Deprecated
+   public static String format() {
 
-		Version version = new Version();
-		return version.getLongFormat();
-	}
-	
-	/**
-	 * @deprecated Use method getBuildId 
-	 */
-	public static String getBuild() {
+      final Version version = new Version();
+      return version.getFormat();
+   }
 
-		Version version = new Version();
-		return version.getBuildId();
-	}
-	
-	private void loadVersion() {
-		if (!loaded) {
-			try {
-				Properties props=new Properties();
-				props.load(FPanelAbout.class.getResourceAsStream("/package.info"));
-				String version = props.getProperty("version", "0.0.0");
-				String[] versionSplit = version.split("[.]");
-				MAJOR_VERSION_NUMBER = Integer.parseInt(versionSplit[0]);
-				MINOR_VERSION_NUMBER = Integer.parseInt(versionSplit[1]);
-				RELEASE_NUMBER = Integer.parseInt(versionSplit[2]);
-				STATE = props.getProperty("state", "");
-				props=new Properties();
-				props.load(FPanelAbout.class.getResourceAsStream("/build.number"));
-				BUILD = props.getProperty("build.number", "0");
-				loaded = true;
-			} catch (Exception e) {
-				logger.error("cant_load_properties", e);
-			}
 
-		}
-		
-	}
-	
-	
-	public int getMinor() {
-		loadVersion();
-		return MINOR_VERSION_NUMBER;
-	}
+   /**
+    * @deprecated Use method getLongFormat
+    */
+   @Deprecated
+   public static String longFormat() {
 
-	public int getMajor() {
-		loadVersion();
-		return MAJOR_VERSION_NUMBER;
-	}
+      final Version version = new Version();
+      return version.getLongFormat();
+   }
 
-	public int getRelease() {
-		loadVersion();
-		return RELEASE_NUMBER;
-	}
 
-	public String getBuildId() {
-		loadVersion();
-		return BUILD;
-	}
+   /**
+    * @deprecated Use method getBuildId
+    */
+   @Deprecated
+   public static String getBuild() {
 
-	public String getState() {
-		loadVersion();
-		return STATE;
-	}
+      final Version version = new Version();
+      return version.getBuildId();
+   }
 
-	public String toString() {
-		return getLongFormat();		
-	}
 
-    public String getFormat() {
-		return this.getMajor() +"."+this.getMinor()+"."+this.getRelease();
-    }
+   private void loadVersion() {
+      if (!loaded) {
+         try {
+            Properties props = new Properties();
+            props.load(FPanelAbout.class.getResourceAsStream("/package.info"));
+            final String version = props.getProperty("version", "0.0.0");
+            final String[] versionSplit = version.split("[.]");
+            MAJOR_VERSION_NUMBER = Integer.parseInt(versionSplit[0]);
+            MINOR_VERSION_NUMBER = Integer.parseInt(versionSplit[1]);
+            RELEASE_NUMBER = Integer.parseInt(versionSplit[2]);
+            STATE = props.getProperty("state", "");
+            props = new Properties();
+            props.load(FPanelAbout.class.getResourceAsStream("/build.number"));
+            BUILD = props.getProperty("build.number", "0");
+            loaded = true;
+         }
+         catch (final Exception e) {
+            logger.error("cant_load_properties", e);
+         }
 
-    public String getLongFormat() {
-		return this.getMajor() + "." + this.getMinor() + "." + this.getRelease() + 
-		" " + this.getState() + " (Build " + this.getBuildId() + ")";
-    }
+      }
+
+   }
+
+
+   public int getMinor() {
+      loadVersion();
+      return MINOR_VERSION_NUMBER;
+   }
+
+
+   public int getMajor() {
+      loadVersion();
+      return MAJOR_VERSION_NUMBER;
+   }
+
+
+   public int getRelease() {
+      loadVersion();
+      return RELEASE_NUMBER;
+   }
+
+
+   public String getBuildId() {
+      loadVersion();
+      return BUILD;
+   }
+
+
+   public String getState() {
+      loadVersion();
+      return STATE;
+   }
+
+
+   @Override
+   public String toString() {
+      return getLongFormat();
+   }
+
+
+   public String getFormat() {
+      return this.getMajor() + "." + this.getMinor() + "." + this.getRelease();
+   }
+
+
+   public String getLongFormat() {
+      return this.getMajor() + "." + this.getMinor() + "." + this.getRelease() + " " + this.getState() + " (Build "
+             + this.getBuildId() + ")";
+   }
+
+
+   public boolean isGvSIGCE() {
+
+      return true;
+
+   }
 
 }
