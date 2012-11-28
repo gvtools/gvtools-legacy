fff1c73258c508c6863925822ac04a74b0f8377a
fergonco <fernando.gonzalez@geomati.co>
2012-11-28 10:03:17 +0100

    MapControls not instantiated by injection

--- extensions/main/src/main/java/org/gvsig/inject/ExtModule.java
+++ extensions/main/src/main/java/org/gvsig/inject/ExtModule.java
@@ -1,21 +1,10 @@
 package org.gvsig.inject;
 
-import com.google.inject.Provider;
-import com.iver.cit.gvsig.fmap.MapControl;
 
 public class ExtModule extends LibModule {
 
 	@Override
 	protected void configure() {
 		super.configure();
-		bind(MapControl.class).toProvider(new Provider<MapControl>() {
-			@Override
-			public MapControl get() {
-				MapControl control = new MapControl();
-				InjectorSingleton.getInjector().injectMembers(control);
-				control.init();
-				return control;
-			}
-		});
 	}
 }
